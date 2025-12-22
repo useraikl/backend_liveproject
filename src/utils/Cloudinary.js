c505eaa5b8bdcf4232b337876821d850681192b1
Ketan Laddha <Ketan@xyz.com>
2025-12-22 19:06:13 +0530

    all setup configured

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,26 @@
+import {v2} from "cloudinary";
+import fs from "fs"
+
+  // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file uploaded successfully
+        console.log("File uploaded on cloudinary successfully:", response.url);
+        return response;
+      }
+      catch (error) {
+        fs.unlinkSync(localFilePath); //remove the file from local uploads folder as operation failed
+        return null;
+      }
+ }
