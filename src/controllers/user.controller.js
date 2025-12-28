e8f7132988619aed4d246e11efda0192cdb36fb7
Ketan Laddha <Ketan@xyz.com>
2025-12-28 23:59:02 +0530

    Adds user channel profile and watch history endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -252,7 +252,12 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
    return res
    .status(200)
-   .json(200,req.user,"Current user fetched successfully")
+   .json( 
+    new ApiResponse(
+    200,
+    req.user,
+    "Current user fetched successfully"
+  ))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -262,7 +267,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400,"All fields are required to update")
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set:{
@@ -359,5 +364,141 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 })
 
 
+const getUserChannelProfile= asyncHandler(async (req, res) => {
+  const { username } = req.params
 
-export { registerUser , loginUser , loggedOutUser , refreshAccessToken  , changeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , updateUserCoverImage };
+  if(!username?.trim()){
+    throw new ApiError(400,"Username is missing")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: { 
+        username: username?.toLowerCase() 
+      }
+    },
+    {
+      $lookup: {
+           from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers"
+      }
+    },
+
+    {
+      $lookup: {
+           from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+      }
+    },
+
+    {
+      $addFields: {
+
+        subscribersCount: { $size: "$subscribers" },
+        channelsSubscribedToCount: { $size: "$subscribedTo" },
+        isSubscribed: {
+          $cond:{
+            if:{
+              $in:[req.user?._id, "$subscribers.subscriber"],
+            },
+            then:true,
+            else:false
+          }
+        }
+       }
+     },
+
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+        createdAt: 1
+      }
+    }
+
+  ])
+
+   if(!channel?.length){
+    throw new ApiError(404,"Channel not found")
+   }
+
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(
+      200,
+      channel[0],
+      "User Channel profile fetched successfully"
+    )
+  )
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+   const user= await User.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(req.user?._id) }
+
+    },
+
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project:{
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+          }
+          },
+
+          {
+              $addFields: {
+                owner: { $arrayElemAt: ["$owner", 0] }
+              }
+
+          }
+        ]
+      }
+    }
+
+
+    ])
+
+    return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+      )
+    )
+})
+
+
+export { registerUser , loginUser , loggedOutUser , refreshAccessToken  , changeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , updateUserCoverImage ,getUserChannelProfile,getWatchHistory};
